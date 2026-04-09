f20370ff6fd93b9938a6732180b6a5940007c12b
xavi <xavi.bellatriu@dxc.com>
2026-04-09 18:58:40 +0200

    [ADD] add configuration files for phpMyAdmin and custom PHP settings

---
+++ conf/config.user.inc.php
@@ -0,0 +1,5 @@
+<?php
+
+$cfg['ShowPhpInfo'] = true; // Adds a link to phpinfo() on the home page
+$cfg['LoginCookieValidity'] = 28800; // Login cookie validity
+$cfg['TablePrimaryKeyOrder'] = 'DESC';
